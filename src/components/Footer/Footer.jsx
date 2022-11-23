5a1bd4adbb7de1150a562bd57042062b0270f79e
Gavin Stirling <gavinstir@gmail.com>
2022-11-23 21:56:28 +0000

    Started styling basics

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,11 @@ import React from "react";
 import "./Footer.scss";
 
 const Footer = () => {
-    return <footer className="footer">Footer</footer>;
+    return (
+        <footer className="footer">
+            <div>Footer</div>
+        </footer>
+    );
 };
 
 export default Footer;
